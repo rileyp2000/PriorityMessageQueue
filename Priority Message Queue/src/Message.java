fd0c4dce9a39ca412e3482e94948df8b946e7dcc
rileyp2000 <rileyp@bishopireton.org>
2018-04-21 22:45:36 -0400

    i think it mostly works

--- Priority Message Queue/src/Message.java
+++ Priority Message Queue/src/Message.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * 
  * @author rileyp
@@ -5,11 +7,13 @@
  */
 public class Message implements Comparable{
 	private int priority, arrival;
+	private static final Random R = new Random(2000);
 	
-	public Message() {
-		priority = ((int)Math.random()*4);
-		arrival = 0;
+	public Message(int a) {
+		priority = R.nextInt(5);
+		arrival = a;
 	}
+	
 	
 	public int getArrival() {
 		return arrival;
@@ -19,7 +23,11 @@ public class Message implements Comparable{
 		return priority;
 	}
 	
-
+	@Override
+	public String toString(){
+		return "Message of priority " + priority + ", Arrived at " + arrival;
+	}
+	
 	@Override
 	public int compareTo(Object m) {
 		return priority - ((Message) m).getPriority();
--- Priority Message Queue/src/MessagePriorityQueue.java
+++ Priority Message Queue/src/MessagePriorityQueue.java
@@ -9,15 +9,62 @@ import java.util.Queue;
  */
 public class MessagePriorityQueue {
 	private ArrayList<Queue<Message>> priorities;
-	
+
 	public MessagePriorityQueue() {
-		for(int i = 0; i < 5; i++)
+		priorities = new ArrayList<Queue<Message>>();
+		for (int i = 0; i < 5; i++)
 			priorities.add(new PriorityQueue<Message>());
+	}
+
+	public void sort(Message m) {
+		System.out.println("S " + m);
+		int pri = m.getPriority();
+		priorities.get(pri).add(m);
+	}
+
+	public void processMessages(int cur) {
+		for (Queue<Message> q : priorities) {
+			if (q.peek() != null) {
+				if (cur - q.peek().getArrival() > 4) {
+					System.out.println(q.remove());
+					return;
+				}
+			}
+		}
+	}
+
+	public int prefill() {
+		int i = 1;
+		for (i=1; i <= 15; i++) {
+			Message m = new Message(i);
+			sort(m);
+		}
+		return i;
+	}
+	
+	public void removeRest(int i){
+		while(priorities.get(0).peek() != null || priorities.get(1).peek() != null 
+				|| priorities.get(2).peek() != null || priorities.get(3).peek() != null 
+					|| priorities.get(4).peek() != null){
+			processMessages(i);
+			i++;
+		}
+	
 	}
 	
 	public static void main(String[] args) {
-		for(int i = 0; i < 20; i++) {
-			
+		MessagePriorityQueue sim = new MessagePriorityQueue();
+		int i = sim.prefill();
+		
+		for (i = i+1; i <= 35; i++) {
+			Message m = new Message(i);
+			sim.sort(m);
+			sim.processMessages(i);
 		}
+		sim.removeRest(i);
+		/*
+		 * for(int i = 1; i < 101; i++){ System.out.println(new Message(i)); }
+		 */
+
 	}
 }
