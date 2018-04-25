f99d180861728e3abaae0526df8ef57e38acb60a
rileyp2000 <rileyp@bishopireton.org>
2018-04-24 22:26:48 -0400

    fixed javadocs

--- Priority Message Queue/src/TimeData.java
+++ Priority Message Queue/src/TimeData.java
@@ -1,4 +1,8 @@
-
+/**
+ * This class represents the data of a removed message
+ * @author Patrick
+ *
+ */
 public class TimeData {
 	private int timeRemoved, waitTime;
 	
@@ -8,25 +12,39 @@ public class TimeData {
 		
 	}
 	
+	@Override
 	public String toString(){
 		return "R: " + timeRemoved + " W:" + waitTime;
 	}
 
+	/**
+	 * @return the timeRemoved
+	 */
 	public int getTimeRemoved() {
 		return timeRemoved;
 	}
 
+	/**
+	 * @return the waitTime
+	 */
 	public int getWaitTime() {
 		return waitTime;
 	}
 
+	/**
+	 * @param timeRemoved the timeRemoved to set
+	 */
 	public void setTimeRemoved(int timeRemoved) {
 		this.timeRemoved = timeRemoved;
 	}
 
+	/**
+	 * @param waitTime the waitTime to set
+	 */
 	public void setWaitTime(int waitTime) {
 		this.waitTime = waitTime;
 	}
+	
 	
 	
 }
